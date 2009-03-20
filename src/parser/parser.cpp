f1c87190e501aa7f41c02687907c10bf41dd066e
bchisham <bchisham@fc0f7330-10d8-11de-8c37-e1423f7a4931>
2009-03-20 19:49:54 +0000

    added length attribute to edge. added otu attribute to row added symbol tables for state, states, and nodes to use while parsing. filled in more implementations for element processing in the parser.

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -24,7 +24,7 @@
 
 using namespace std;
 using namespace Glib;
-
+//<------------------Tool-kit for walking the parse tree------------->
 //root processing.
 //Process the root/nexml node.
 static NeXML::Nexml* process_root( xmlpp::Node* node );
@@ -56,6 +56,7 @@ static NeXML::Edge* process_edge( xmlpp::Node* node );
 static NeXML::Edge* process_rootedge( xmlpp::Node* node );
 //process a state definition element.
 static NeXML::State* process_state( xmlpp::Node* node );
+static void          process_member( xmlpp::Node* node, NeXML::StateSet* stateset );
 //process a character declaraction.
 static NeXML::Character*   process_char( xmlpp::Node* node );
 //process a cell.
@@ -63,11 +64,29 @@ static NeXML::Cell* process_cell( xmlpp::Node* node );
 //process an annotation element.
 static NeXML::Annotation* process_annotation( xmlpp::Node* node );
 static NeXML::Otu* process_otu( xmlpp::Node* node );
-
-
+//<-------Keep some info handy while parsing----------------->
+//Elements compute their own id's based on their contents which
+//might not match up with the id's they have in the original document.
+static map< Glib::ustring, NeXML::State* > states_processed_;
+static map< Glib::ustring, NeXML::Character* > chars_processed_;
+static map< Glib::ustring, NeXML::States* > states_blocks_processed_;
+static map< Glib::ustring, NeXML::Network* > trees_processed_;
+static map< Glib::ustring, NeXML::Node* > nodes_processed_;
+static map< Glib::ustring, NeXML::Otu* > otus_processed_;
+//<---------------------------------------------------------->
+/*
+ * Set things up and start walking the parse tree.
+ */
 NeXML::Nexml* NeXML::DOM_PARSER::parser( const Glib::ustring& src ){
    try {
      //initialize
+     states_processed_ = map< Glib::ustring, NeXML::State* >();
+     chars_processed_ = map< Glib::ustring, NeXML::Character* >();
+     states_blocks_processed_ = map< Glib::ustring, NeXML::States* >();
+     trees_processed_ = map< Glib::ustring, NeXML::Network* >();
+     nodes_processed_ = map< Glib::ustring, NeXML::Node* >();
+     otus_processed_ = map< Glib::ustring, NeXML::Otu* >();
+
      xmlpp::DomParser parser;
      xmlpp::Node* parse_tree;
      //parser.set_validate();
@@ -75,8 +94,8 @@ NeXML::Nexml* NeXML::DOM_PARSER::parser( const Glib::ustring& src ){
      
      //parse and process
      parser.parse_file( src );
-
      if ( parser ){
+       //parsing was successful
        xmlpp::Node* parsetree = parser.get_document()->get_root_node();
         NeXML::Nexml* nexml_doc =  process_root( parsetree );
         return nexml_doc;
@@ -87,12 +106,14 @@ NeXML::Nexml* NeXML::DOM_PARSER::parser( const Glib::ustring& src ){
    }
    return NULL;
 }
-
-
+/*
+ * Handle processing of the document root. 
+ */
 NeXML::Nexml* process_root( xmlpp::Node* node ){
   if (node){
      NeXML::Nexml* ret = new NeXML::Nexml();
      xmlpp::Node::NodeList list = node->get_children();
+     //process child elements. we expect otus, characters (matrices), trees, and maybe annotations.
      for (xmlpp::Node::NodeList::iterator it = list.begin(); it != list.end(); ++it ){
          //process children.
          if ((*it)->get_name() == NeXML::OTUS_TAG){
@@ -111,12 +132,13 @@ NeXML::Nexml* process_root( xmlpp::Node* node ){
              std::cerr << "Unknown element: " << (*it)->get_name() << std::endl;
          }
      }
-
      return ret;
   }
   return NULL;
 }
-
+/*
+ * Handle processing of the otus block. 
+ */
 static NeXML::Otus* process_otus( xmlpp::Node* node ){
   if ( node ){
     ustring id = dynamic_cast< xmlpp::Element* >( node )->get_attribute( "id" )->get_value();
@@ -137,17 +159,22 @@ static NeXML::Otus* process_otus( xmlpp::Node* node ){
   }
   return NULL;
 }
-
+/*
+ * Processes a single otu.
+ */
 NeXML::Otu* process_otu( xmlpp::Node* node ){
   if ( node ){
      ustring label = dynamic_cast< xmlpp::Element* >( node )->get_attribute( "label" )->get_value();
+     ustring id    = dynamic_cast< xmlpp::Element* >( node )->get_attribute( "id" )->get_value();
      NeXML::Otu* ret = new NeXML::Otu( label );
+     otus_processed_[ id ] = ret;
      return ret;
   }
   return NULL;
 }
-
-
+/*
+ * Process a characters block.
+ */
 NeXML::Characters* process_characters( xmlpp::Node* node, const NeXML::Otus* otus ){ 
   if (node){
     //get the type of characters type
@@ -171,8 +198,10 @@ NeXML::Characters* process_characters( xmlpp::Node* node, const NeXML::Otus* otu
   }
   return NULL; 
 }
-
-NeXML::Trees*     process_trees( xmlpp::Node* node, const NeXML::Otus* otus ){
+/*
+ * process the tress block.
+ */
+NeXML::Trees* process_trees( xmlpp::Node* node, const NeXML::Otus* otus ){
     if ( node ){
         NeXML::Trees* ret = new NeXML::Trees( otus );
         xmlpp::Node::NodeList list = node->get_children();
@@ -194,9 +223,14 @@ NeXML::Trees*     process_trees( xmlpp::Node* node, const NeXML::Otus* otus ){
     }
     return NULL;
 }
-
-NeXML::Tree*      process_tree( xmlpp::Node* node ){ 
+/*
+ * Process a single tree.
+ */
+NeXML::Tree* process_tree( xmlpp::Node* node ){ 
   if (node){
+    //reset the nodes map.
+    nodes_processed_ = map< Glib::ustring, NeXML::Node* >();
+
     ustring type = dynamic_cast< xmlpp::Element* >( node )->get_attribute( "type" )->get_value();
     ustring label = dynamic_cast< xmlpp::Element* >( node )->get_attribute( "label" )->get_value();
     NeXML::Tree* ret = new NeXML::Tree(label, type);
@@ -221,7 +255,10 @@ NeXML::Tree*      process_tree( xmlpp::Node* node ){
   }
   return NULL; 
 }
-NeXML::Network*   process_network( xmlpp::Node* node ){ 
+/*
+ * Process a network
+ */
+NeXML::Network* process_network( xmlpp::Node* node ){ 
   if (node){
   ustring type = dynamic_cast< xmlpp::Element* >( node )->get_attribute( "type" )->get_value();
     ustring label = dynamic_cast< xmlpp::Element* >( node )->get_attribute( "label" )->get_value();
@@ -247,7 +284,9 @@ NeXML::Network*   process_network( xmlpp::Node* node ){
   }
   return NULL; 
 }
-
+/*
+ * Process a format block
+ */
 NeXML::Format* process_format( xmlpp::Node* node ){ 
   if (node){
     NeXML::Format* ret = new NeXML::Format();
@@ -270,12 +309,25 @@ NeXML::Format* process_format( xmlpp::Node* node ){
   }
   return NULL; 
 }
+/*
+ * Process a states block
+ */
 NeXML::States* process_states( xmlpp::Node* node ){ 
   if (node){
+    xmlpp::Node::NodeList l = node->get_children();
+    Glib::ustring id = dynamic_cast< xmlpp::Element* >( node )->get_attribute( "id" )->get_value();
+    NeXML::States* ret = new NeXML::States( id );
+    states_blocks_processed_[ id ] = ret;
+    for ( xmlpp::Node::NodeList::iterator i = l.begin(); i != l.end(); ++i ){
+         ret->addstate( process_state( *i ) );
+    }
 
   }
   return NULL; 
 }
+/*
+ * Process a matrix.
+ */
 NeXML::Matrix* process_matrix( xmlpp::Node* node ){ 
   if (node){
      xmlpp::Node::NodeList l = node->get_children();
@@ -295,55 +347,148 @@ NeXML::Matrix* process_matrix( xmlpp::Node* node ){
   }
   return NULL; 
 }
-
+/*
+ * Process a single row.
+ */
 NeXML::Row* process_row( xmlpp::Node* node ){ 
   if (node){
-
+       if (xmlpp::Element* ele = dynamic_cast< xmlpp::Element* >( node )){
+            NeXML::Otu* otu = otus_processed_[ ele->get_attribute( "otu" )->get_value() ];
+            xmlpp::Attribute* lab = ele->get_attribute( "label" );
+            ustring label = "";
+            if (lab){  label = lab->get_value(); }
+            NeXML::Row* ret = new NeXML::Row( otu, label );
+            xmlpp::Node::NodeList l = node->get_children();
+            for ( xmlpp::Node::NodeList::iterator i = l.begin(); i != l.end(); ++i ){
+                ret->addcell( process_cell( *i ) );
+            }
+            return ret;
+       }
   }
   return NULL;
 }
 //lowest level
+/*
+ * Process a single node.
+ */
 NeXML::Node* process_node( xmlpp::Node* node ){ 
   if (node){
-
+     if (xmlpp::Element* ele = dynamic_cast< xmlpp::Element* >( node )){
+       ustring id = ele->get_attribute( "id" )->get_value();
+       //get optional attributes
+       xmlpp::Attribute* label = ele->get_attribute( "label" );
+       xmlpp::Attribute* otu   = ele->get_attribute( "otu"   );
+       xmlpp::Attribute* isroot = ele->get_attribute( "root" );
+       NeXML::Otu* otup;
+       ustring labelstr = "";
+       bool root = false;
+       if ( label ){ labelstr = label->get_value(); }
+       if ( otu ){ otup = otus_processed_[ otu->get_value() ]; }
+       if ( isroot ){ root = isroot->get_value() == "true"; }
+       NeXML::Node* ret = new NeXML::Node( labelstr, otup, root );
+       nodes_processed_[ id ] = ret;
+       return ret;
+     }
   }
   return NULL; 
 }
+/*
+ * Process a single edge.
+ */
 NeXML::Edge* process_edge( xmlpp::Node* node ){ 
   if (node){
-
+       if (xmlpp::Element* ele = dynamic_cast< xmlpp::Element* >(node)){
+            NeXML::Node* src = nodes_processed_[ ele->get_attribute("source")->get_value() ];
+            NeXML::Node* target = nodes_processed_[ ele->get_attribute("target")->get_value() ];
+            xmlpp::Attribute* len = ele->get_attribute( "length" );
+            ustring length = "";
+            if (len){ length = len->get_value(); }
+            NeXML::Edge* ret = new NeXML::Edge( src, target, length);
+            return ret;
+       }
   }
   return NULL;
 }
-
+/*
+ * Process a rootedge.
+ */
 NeXML::Edge* process_rootedge( xmlpp::Node* node ){
   if ( node ){
 
   }
   return NULL;
 }
-
+/*
+ * Process a state.
+ */
 NeXML::State* process_state( xmlpp::Node* node ){ 
+  NeXML::State* ret = NULL;
   if (node){
-  
-  
+    xmlpp::Element* ele = dynamic_cast< xmlpp::Element* >( node );
+        if ( ele ){
+       Glib::ustring id = ele->get_attribute( "id" )->get_value();
+       Glib::ustring symbol = ele->get_attribute( "symbol" )->get_value();
+       if (node->get_name() == NeXML::POLY_TAG){
+          ret = new NeXML::PolymorphicState( id, symbol );
+       }
+       else if (node->get_name() == NeXML::UNCERT_TAG){
+          ret = new NeXML::UncertainState( id, symbol );
+       }
+       else if ( node->get_name() == NeXML::STATE_TAG){
+          ret = new NeXML::State( id, symbol );
+          //add to the states symbol table.
+          states_processed_[ id ] = ret;
+       }
+    }
+  }
+  return ret;
+}
+/*
+ * Process a member element for polymorphic and uncertain states.
+ */
+void process_member( xmlpp::Node* node, NeXML::StateSet* stateset ){
+  if (node && stateset){
+    xmlpp::Node::NodeList l = node->get_children();
+    for ( xmlpp::Node::NodeList::iterator i = l.begin(); i != l.end(); ++i ){
+        Glib::ustring state = dynamic_cast< xmlpp::Element* >( *i )->get_attribute( "state" )->get_value();
+        //link back to the previously processed state. 
+        stateset->addstate( states_processed_[ state ] );
+    }
+  }
+  return;
+}
+/*
+ * Process a char element.
+ */
+NeXML::Character* process_char( xmlpp::Node* node ){ 
+  if (node){
+       if (xmlpp::Element* ele = dynamic_cast< xmlpp::Element* >(  node )){
+          Glib::ustring id = ele->get_attribute( "id" )->get_value();
+          Glib::ustring state = ele->get_attribute( "states" )->get_value();
+          NeXML::Character* ret =  new NeXML::Character( states_blocks_processed_[ state ] );
+          chars_processed_[ id ] = ret;
+          return ret;
+       }
   }
   return NULL;
 }
-NeXML::Character*   process_char( xmlpp::Node* node ){ 
-  if (node){
-
-  }
-  return NULL;
-}
-
+/*
+ * Process a cell.
+ */
 NeXML::Cell* process_cell( xmlpp::Node* node ){ 
   if (node){
-
+      if (xmlpp::Element* ele = dynamic_cast< xmlpp::Element* >( node )){
+           Glib::ustring ch = ele->get_attribute( "char" )->get_value();
+           Glib::ustring st = ele->get_attribute( "state" )->get_value();
+           NeXML::Cell* ret = new NeXML::Cell( chars_processed_[ch], states_processed_[ st ] );
+           return ret;
+      }
   }
   return NULL; 
 }
-
+/*
+ * Process an annotation.
+ */
 NeXML::Annotation* process_annotation( xmlpp::Node* node ){ 
   if (node){
 
