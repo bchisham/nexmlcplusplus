0728c654cf67e583ffb1827450ce4e4e47dd0023
bchisham <bchisham@fc0f7330-10d8-11de-8c37-e1423f7a4931>
2009-03-24 20:18:47 +0000

    created a demo reader application. started some testing.

---
+++ src/examples/example_reader.cpp
@@ -0,0 +1,55 @@
+/**
+ * Driver program to demonstrate reading with the NeXMLCPP library.
+ * Brandon Chisham
+ * 24 March 2009
+ */
+
+#include <nexml.hpp>
+#include <parser/parser.hpp>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+struct Environment {
+   string infile_name;
+   string outfile_name;
+};
+
+
+const string INFLAG = "-i";
+
+void useage();
+Environment process_args( int argc, char** argv );
+
+
+int main( int argc, char** argv ){
+   Environment env = process_args( argc, argv );
+   NeXML::Nexml* doc = NeXML::DOM_PARSER::parser( env.infile_name );
+
+   return 0;
+}
+
+
+void useage(){
+  cout << "./example_reader -i FILE_NAME" << endl;
+  return;
+}
+
+Environment process_args( int argc, char** argv ){
+ //minimum number of arguments
+  const unsigned int EXPECTED_ARGS = 2;
+  if ( argc < EXPECTED_ARGS ){ 
+    useage(); 
+    exit( 0 );
+  }
+  Environment ret;
+  for ( unsigned int i = 1; i < argc-1; ++i ){
+     if ( argv[i] == INFLAG ){
+         ret.infile_name = argv[ i + 1 ];
+     }
+  }
+  return ret;
+}
--- src/trees.cpp
+++ src/trees.cpp
@@ -1,12 +1,14 @@
 #include "trees.hpp"
 #include "tags.hpp"
 
+using namespace NeXML;
+
 std::ostream& Trees::serialize( std::ostream& out )const{
   
    out << "<" << TREES_TAG << " id=\"" << getid() << "\" otus=\"" << otus_->getid() << "\">\n";
    out << dynamic_cast< const Annotable* >( this );
    for ( unsigned int i = 0; i < trees_.size(); ++i ){
-       out << tress_.at( i );
+       out << trees_.at( i );
    }
    out << "</" << TREES_TAG << ">\n";
    return out;
